ecc20526deb55f339cd39eca0c4770ecf5ab14c0
Jarvis <jarvis@whoza.ai>
2026-06-28 02:22:58 +0800

    fix(build): remove .next build output from git, restore proper .gitignore

--- .next/static/chunks/app/refund-policy/page-159591afcc9794cf.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[1366,1663,3347,6990],{69403:(e,s,n)=>{Promise.resolve().then(n.bind(n,80353)),Promise.resolve().then(n.bind(n,50163))}},e=>{e.O(0,[3993,290,8643,8441,1255,7358],()=>e(e.s=69403)),_N_E=e.O()}]);
--- .next/static/n9puqhCHWwXzmMW4x28bN/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F[location]","\u002Fblog\u002F[slug]","\u002Ftrade\u002F[trade]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
